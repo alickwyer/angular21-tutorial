94c9c16c158aa283dca943029fe016388ddb59b4
Mohit Kumar <mks.djcet@gmail.com>
2025-11-23 19:37:39 +0530

    Updated Hello World code

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-tutorial');
+  name = "Angular Tutorial";
+  username = "mohitkumar";
+  isAdmin = true;
+  count = 3;
 }
